3003bcef73c7adc5d7b05914c2a303cbcef6c6f2
arthurs14 <arthurcs426@gmail.com>
2021-07-26 14:15:42 -0700

    first example of using aasync await to get the rates from Fixer API

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 import axios from 'axios';
-import { FIXER_API, REST_COUNTRIES_API } from './api-keys';
+import { FIXER_API, REST_COUNTRIES_API } from './api-keys.js';
+
+// Async/Await
+// Returns Promises
+
+// Fetch Data about currencies
+const getExchangeRate = async (fromCurrency, toCurrency) => {
+  const { data: { rates } } = await axios.get(FIXER_API);
+
+  console.log(rates.USD);
+
+  const euro = 1 / rates[fromCurrency];
+  console.log('conversion USD to Euro:', euro);
+
+  const exchangeRate = euro * rates[toCurrency];
+  console.log('conversion:', exchangeRate);
+};
+
+getExchangeRate('USD', 'AUD');
+
+// Fetch Data about countries
+
+// Output data
