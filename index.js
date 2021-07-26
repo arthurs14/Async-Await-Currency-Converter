71e11e4c5dc22c01f35ec9d512ef3147bd63c75b
arthurs14 <arthurcs426@gmail.com>
2021-07-26 14:29:48 -0700

    made our await calls simultaneously so that it runs more efficient

--- index.js
+++ index.js
@@ -10,15 +10,37 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
 
   console.log(rates.USD);
 
-  const euro = 1 / rates[fromCurrency];
-  console.log('conversion USD to Euro:', euro);
+  const currency = 1 / rates[fromCurrency];
+  const exchangeRate = currency * rates[toCurrency];
 
-  const exchangeRate = euro * rates[toCurrency];
-  console.log('conversion:', exchangeRate);
+  return exchangeRate;
+
 };
 
-getExchangeRate('USD', 'AUD');
 
 // Fetch Data about countries
+const getCountries = async (currencyCode) => {
+  const { data } = await axios.get(`${REST_COUNTRIES_API}/${currencyCode}`); 
+
+  const countries = data.map(({ name }) => name);
+
+  return countries;
+};
+
+const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+  fromCurrency = fromCurrency.toUpperCase();
+  toCurrency = toCurrency.toUpperCase();
+
+  // Runs simulataneously
+  const [countries, exchangeRate] = await Promise.all([
+    getCountries(toCurrency),
+    getExchangeRate(fromCurrency, toCurrency),
+  ]);
+
+  console.log(countries);
+  console.log(exchangeRate);
+};
+
+convertCurrency('AUD', 'USD', 20);
 
 // Output data
