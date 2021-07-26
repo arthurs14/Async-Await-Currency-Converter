93c37c80a258adcd31c2d255f61c579847bd4953
arthurs14 <arthurcs426@gmail.com>
2021-07-26 14:39:45 -0700

    added some try catch brackets to the await functions

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ import { FIXER_API, REST_COUNTRIES_API } from './api-keys.js';
 
 // Fetch Data about currencies
 const getExchangeRate = async (fromCurrency, toCurrency) => {
-  const { data: { rates } } = await axios.get(FIXER_API);
+  try {
+    const { data: { rates } } = await axios.get(FIXER_API);
+  
+    const currency = 1 / rates[fromCurrency];
+    const exchangeRate = currency * rates[toCurrency];
+  
+    return exchangeRate;
 
-  console.log(rates.USD);
-
-  const currency = 1 / rates[fromCurrency];
-  const exchangeRate = currency * rates[toCurrency];
-
-  return exchangeRate;
-
+  } catch (error) {
+    throw new Error(`Unable to get currecy ${fromCurrency} and ${toCurrency}`);
+  }
 };
 
 // Fetch Data about countries
 const getCountries = async (currencyCode) => {
-  const { data } = await axios.get(`${REST_COUNTRIES_API}/${currencyCode}`); 
-
-  const countries = data.map(({ name }) => name);
-  console.log('countries: \n', countries);
-
-  return countries;
+  try {
+    const { data } = await axios.get(`${REST_COUNTRIES_API}/${currencyCode}`); 
+  
+    const countries = data.map(({ name }) => name);
+  
+    return countries;
+  } catch (error) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`);
+  }
 };
 
 const convertCurrency = async (fromCurrency, toCurrency, amount) => {
@@ -42,7 +47,7 @@ const convertCurrency = async (fromCurrency, toCurrency, amount) => {
   return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
 };
 
-convertCurrency('AUD', 'USD', 20)
+convertCurrency('AUD', 'CAD', 20)
   .then(result => console.log(result))
   .catch(error => console.log(error));
 
