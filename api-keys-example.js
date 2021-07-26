34ab90fd3bcd1d94be4f9be5657d7477ac3b548c
arthurs14 <arthurcs426@gmail.com>
2021-07-26 13:50:45 -0700

    added api keys to project and also added template for api keys

---
+++ api-keys-example.js
@@ -0,0 +1,9 @@
+// Fill in the key for Fixer API by signing up
+// Create an api-keys.js and put the below into there.
+
+// https://fixer.io/
+export const FIXER_API_KEY = "";
+export const FIXER_API = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`;
+
+// https://restcountries.eu
+export const REST_COUNTRIES_API = `https://restcountries.eu/rest/v2/currency`;
---
+++ index.js
@@ -0,0 +1,2 @@
+import axios from 'axios';
+import { FIXER_API, REST_COUNTRIES_API } from './api-keys';
